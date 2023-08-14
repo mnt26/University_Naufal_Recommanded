5bcb38287ced84b79b06c0faa1b4d37b2a8c0691
mnt26 <bluenaufal7@gmail.com>
2023-08-14 15:19:16 +0700

    update 1.0

--- from-handler.php
+++ from-handler.php
@@ -2,26 +2,23 @@
 $name = $_POST['name'];
 $visitor_email = $_POST['email'];
 $subject = $_POST['Subject'];
-$massage = $_POST['Massage'];
+$message = $_POST['Massage'];
 
-
-$email_from = 'info@yourwebsite.com';
+$email_from = 'bluenaufal7@gmail.com'; // Sesuaikan dengan alamat email situs web Anda
 
 $email_subject = 'New Form Submission';
 
 $email_body = "User Name: $name.\n".
-                "User Email: $visitor_email.\n".
-                "Subject : $Subject.\n".
-                "User Massage : $massage.\n";
+              "User Email: $visitor_email.\n".
+              "Subject: $subject.\n".
+              "User Massage: $message.\n";
 
 $to = 'bluenaufal@gmail.com';
 
-$headers = "Form: $email_form \r\n";
-
+$headers = "From: $email_from \r\n";
 $headers .= "Reply-To: $visitor_email \r\n";
 
-mail($to,$email_subject,$email_body,$headers);
+mail($to, $email_subject, $email_body, $headers);
 
 header("Location: contact.html");
-
 ?>
